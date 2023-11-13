b44d8b8bea51b564f8d3a7d1c7dd97a13a7e8912
Kristina Zvolanek <54590158+kristinazvolanek@users.noreply.github.com>
2023-11-13 17:49:18 -0600

    Automatically extract rejected and accepted components. Account for extra TRs in CO2 regressor

--- x.PreProc_ME_Conservative_Model.sh
+++ x.PreProc_ME_Conservative_Model.sh
@@ -7,42 +7,78 @@
 #Rules for classifying ICA components on SharePoint: https://nuwildcat.sharepoint.com/:w:/r/sites/FSM-ANVIL/Shared%20Documents/ResourcesGuides/Multi-echo%20ICA%20classification%20rules.docx?d=w2b995b3b0a02463e8198c511bfb3dc7f&csf=1&web=1&e=FjLGmD
 #After running this script, you can use your orthogonalized rejected components as regressors in your GLM for denoising
 
-if [ $# -ne 7 ]
+if [ $# -ne 8 ]
 then
   echo "*****************************************************************************************************"
   echo "Insufficient arguments supplied"
-  echo "Input 1 should be the full path to your ICA mixing matrix (do not include file extension - assumes txt)"
-  echo "Input 2 should be a list of your accepted ICA components (of form [1,2,3])"
-  echo "Input 3 should be a list of your rejected ICA components (of form [1,2,3])"
-  echo "Input 4 should be the full path to the demeaned PETCO2 trace convolved with the HRF (do not include file extension - assumes txt)"
-  echo "Input 5 should be the full path to the demeaned motion parameters (do not include file extension - assumes 1D)"
-  echo "Input 6 should be the Legendre polynomial degree"
-  echo "Input 7 should be the full path to the output directory"
+  echo "Input 1 should be the full path to desc-ICA_mixing.tsv file (include file extension)"
+  echo "Input 2 should be the full path to the manual_classification.tsv file (include file extension)"
+  echo "Input 3 should be the full path to the demeaned PETCO2 trace convolved with the HRF (do not include file extension - assumes .txt)"
+  echo "Input 4 should be the number of extra TRs added before and after the scan (assumes equal # added before and after)."
+  echo "        For example, if 20 TRs were added to both the beginning AND end of the scan (40 extra TRs total), you would enter 20 here."
+  echo "Input 5 should be the original number of TRs in the scan (before extra TR padding)"
+  echo "Input 6 should be the full path to the demeaned motion parameters (do not include file extension - assumes .1D)"
+  echo "Input 7 should be the Legendre polynomial degree"
+  echo "Input 8 should be the full path to the output directory"
   echo "*****************************************************************************************************"
   exit
 fi
 
 #Define outputs
-mixing=${1}
-accepted=${2}
-rejected=${3}
-CO2=${4}
-motion=${5}
-Legendre_degree=${6}
-output_dir=${7}
+ica_mix=${1}
+man_class=${2}
+CO2=${3}
+extraTR=${4}
+nTR=${5}
+motion=${6}
+Legendre_degree=${7}
+output_dir=${8}
 
 cd ${output_dir}
 
-#Concatenate accepted and rejected components
-1dcat ${mixing}.txt${accepted}   > accepted_tr.1D
-1dcat ${mixing}.txt${rejected}   > rejected_tr.1D
-1dtranspose rejected_tr.1D > rejected.1D
+
+# Find REJECTED component numbers from tedana file
+# Rica code outputs classifications to column 17
+manRej=`cut -f17 ${man_class} | tail -n +2 | grep -n rejected | cut -d : -f1 | tr "\n" " "`
+
+manRejArr=( $manRej ) #change string to array
+for (( i = 0 ; i < ${#manRejArr[@]} ; i++ )) do  (( manRejArr[$i]=${manRejArr[$i]} - 1 )) ; done #subtract 1 from each number to start indexing at 0
+
+printf -v manRejCom '%s,' "${manRejArr[@]}" #separate array by commas instead of spaces
+
+# Create file with REJECTED components
+1dcat ${ica_mix}[${manRejCom[@]}] > ${output_dir}/rejectedTrans.1D
+1dtranspose ${output_dir}/rejectedTrans.1D > ${output_dir}/rejected.1D
+
+
+
+# Find ACCEPTED component numbers from tedana file
+# Rica code outputs classifications to column 17
+manAcc=`cut -f17 ${man_class} | tail -n +2 | grep -n accepted | cut -d : -f1 | tr "\n" " "`
+
+manAccArr=( $manAcc ) #change string to array
+for (( i = 0 ; i < ${#manAccArr[@]} ; i++ )) do  (( manAccArr[$i]=${manAccArr[$i]} - 1 )) ; done #subtract 1 from each number to start indexing at 0
+
+printf -v manAccCom '%s,' "${manAccArr[@]}" #separate array by commas instead of spaces
+
+# Create file with ACCEPTED components
+1dcat ${ica_mix}[${manAccCom[@]}] > ${output_dir}/acceptedTrans.1D
+1dtranspose ${output_dir}/acceptedTrans.1D > ${output_dir}/accepted.1D
+
+
+
+# Convert CO2 file to 1D so you can select appropriate volumes (ignore any extra TRs added before/after)
+1d_tool.py -infile ${CO2}.txt -write ${CO2}.1D
+extraTR=20 # this is also the start volume
+nTR=265 
+last=$((${nTR}+${extraTR}-1)) #python indexing starts at 0
+echo  $last
 
 #Orthogonalize rejected components to accepted components, CO2 trace, motion, and polynomials
-3dTproject -ort accepted_tr.1D \
--ort ${CO2}.txt \
+3dTproject -ort acceptedTrans.1D \
+-ort ${CO2}.1D"{$extraTR..$last}" \
 -ort ${motion}.1D \
 -polort ${Legendre_degree}  \
--prefix rejected_ort_tr.1D \
+-prefix rejectedTrans_ort.1D \
 -input rejected.1D
-1dtranspose rejected_ort_tr.1D > rejected_ort.1D
+1dtranspose rejectedTrans_ort.1D > rejected_ort.1D
